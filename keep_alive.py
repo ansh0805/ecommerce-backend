5b80334b62c1005ec7f1827b6695b5db4ed4efc3
Ansh <anshlakkad481@gmail.com>
2026-05-23 17:40:22 +0530

    fix port

---
+++ keep_alive.py
@@ -0,0 +1,8 @@
+﻿import urllib.request, time
+while True:
+    try:
+        urllib.request.urlopen("https://web-production-6ceec.up.railway.app/")
+        print("Server alive!")
+    except Exception as e:
+        print(f"Failed: {e}")
+    time.sleep(300)
